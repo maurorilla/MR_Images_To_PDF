7698e9c5d9ff27a8e2763b37e4aef699e2a179be
Mauro Rilla <mauro@rilla.it>
2020-04-19 14:52:02 +0200

    Add project files.

---
+++ MR_PDF_Tools/MainForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MR_PDF_Tools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnJpgToPDF = new System.Windows.Forms.Button();
+            this.btnAddJPG = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.pPreview = new System.Windows.Forms.PictureBox();
+            this.FileList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.lblsize = new System.Windows.Forms.Label();
+            this.ckFolder = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnJpgToPDF
+            // 
+            this.btnJpgToPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnJpgToPDF.Location = new System.Drawing.Point(734, 446);
+            this.btnJpgToPDF.Margin = new System.Windows.Forms.Padding(2);
+            this.btnJpgToPDF.Name = "btnJpgToPDF";
+            this.btnJpgToPDF.Size = new System.Drawing.Size(80, 39);
+            this.btnJpgToPDF.TabIndex = 0;
+            this.btnJpgToPDF.Text = "Create PDF";
+            this.btnJpgToPDF.UseVisualStyleBackColor = true;
+            this.btnJpgToPDF.Click += new System.EventHandler(this.btnJpg_Click);
+            // 
+            // btnAddJPG
+            // 
+            this.btnAddJPG.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAddJPG.Location = new System.Drawing.Point(8, 446);
+            this.btnAddJPG.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAddJPG.Name = "btnAddJPG";
+            this.btnAddJPG.Size = new System.Drawing.Size(112, 39);
+            this.btnAddJPG.TabIndex = 3;
+            this.btnAddJPG.Text = "Add Images ...";
+            this.btnAddJPG.UseVisualStyleBackColor = true;
+            this.btnAddJPG.Click += new System.EventHandler(this.btnAddJPG_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveUp.Location = new System.Drawing.Point(460, 10);
+            this.btnMoveUp.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(43, 39);
+            this.btnMoveUp.TabIndex = 5;
+            this.btnMoveUp.Text = "Move Up";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDown.Location = new System.Drawing.Point(460, 396);
+            this.btnDown.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(43, 39);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.Text = "Mode Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRemove.Location = new System.Drawing.Point(140, 446);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(99, 39);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove From List";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // pPreview
+            // 
+            this.pPreview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pPreview.BackColor = System.Drawing.Color.White;
+            this.pPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pPreview.Location = new System.Drawing.Point(508, 10);
+            this.pPreview.Margin = new System.Windows.Forms.Padding(2);
+            this.pPreview.Name = "pPreview";
+            this.pPreview.Size = new System.Drawing.Size(306, 426);
+            this.pPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pPreview.TabIndex = 8;
+            this.pPreview.TabStop = false;
+            // 
+            // FileList
+            // 
+            this.FileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.FileList.GridLines = true;
+            this.FileList.HideSelection = false;
+            this.FileList.Location = new System.Drawing.Point(8, 7);
+            this.FileList.Margin = new System.Windows.Forms.Padding(2);
+            this.FileList.MultiSelect = false;
+            this.FileList.Name = "FileList";
+            this.FileList.Size = new System.Drawing.Size(449, 429);
+            this.FileList.TabIndex = 12;
+            this.FileList.UseCompatibleStateImageBehavior = false;
+            this.FileList.View = System.Windows.Forms.View.Details;
+            this.FileList.SelectedIndexChanged += new System.EventHandler(this.FileList_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FileName";
+            this.columnHeader1.Width = 538;
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRemoveAll.Location = new System.Drawing.Point(344, 446);
+            this.btnRemoveAll.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(112, 39);
+            this.btnRemoveAll.TabIndex = 13;
+            this.btnRemoveAll.Text = "Remove All";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // lblsize
+            // 
+            this.lblsize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblsize.AutoSize = true;
+            this.lblsize.Location = new System.Drawing.Point(508, 441);
+            this.lblsize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblsize.Name = "lblsize";
+            this.lblsize.Size = new System.Drawing.Size(16, 13);
+            this.lblsize.TabIndex = 14;
+            this.lblsize.Text = "...";
+            // 
+            // ckFolder
+            // 
+            this.ckFolder.AutoSize = true;
+            this.ckFolder.Checked = true;
+            this.ckFolder.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckFolder.Location = new System.Drawing.Point(510, 468);
+            this.ckFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.ckFolder.Name = "ckFolder";
+            this.ckFolder.Size = new System.Drawing.Size(136, 17);
+            this.ckFolder.TabIndex = 15;
+            this.ckFolder.Text = "Open Folder after Save";
+            this.ckFolder.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 497);
+            this.Controls.Add(this.ckFolder);
+            this.Controls.Add(this.lblsize);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.FileList);
+            this.Controls.Add(this.pPreview);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnAddJPG);
+            this.Controls.Add(this.btnJpgToPDF);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(840, 536);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MR Images To PDF 1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJpgToPDF;
+        private System.Windows.Forms.Button btnAddJPG;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.PictureBox pPreview;
+        private System.Windows.Forms.ListView FileList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Label lblsize;
+        private System.Windows.Forms.CheckBox ckFolder;
+    }
+}
---
+++ MR_PDF_Tools/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MR_PDF_Tools
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJpg_Click(object sender, EventArgs e)
+        {
+            if (FileList.Items.Count == 0)
+            {
+                MessageBox.Show("Add Images to generate the pdf file.");
+                return;
+            }
+            var SFD = new SaveFileDialog();
+            SFD.FileName = $"PDF_{DateTime.Now:dd_MM_yyyy_HHhh}.pdf";
+            SFD.Filter = "File PDF|*.PDF";
+            if (SFD.ShowDialog() == DialogResult.OK)
+            {
+                var myPath = Path.GetTempPath() + "\\MR_PDF_Tools_tmp\\";
+                if (!Directory.Exists(myPath)) Directory.CreateDirectory(myPath);
+                //Creo una cartella
+                myPath = $"{myPath}{DateTime.Now.ToString("yyyyMMddHHmmss")}\\";
+                if (!Directory.Exists(myPath)) Directory.CreateDirectory(myPath);
+
+                //Copio i file e li ridimensione
+                var i = 1;
+                var MyList = new List<string>();
+                foreach (ListViewItem l1 in FileList.Items)
+                {
+                    var CurrentFile = l1.Text;
+                    var NewFile = myPath + "Pag_" + i + Path.GetExtension(CurrentFile);
+                    File.Copy(CurrentFile, NewFile);
+                    var NewFilePDF = NewFile + ".pdf";
+                    ConvertJPG2PDF(NewFile, NewFilePDF);
+                    File.Delete(NewFile);
+                    i++;
+                    MyList.Add(NewFilePDF);
+                }
+
+                MergeFiles(SFD.FileName, MyList.ToArray());
+                if (ckFolder.Checked) Process.Start(Path.GetDirectoryName(SFD.FileName));
+            }
+        }
+        public static string GetAppPath()
+        {
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+
+        public static void MergeFiles(string destinationFile, string[] sourceFiles)
+        {
+
+            try
+            {
+                int f = 0;
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(sourceFiles[f]);
+                // we retrieve the total number of pages
+                int n = reader.NumberOfPages;
+                //Console.WriteLine("There are " + n + " pages in the original file.");
+                // step 1: creation of a document-object
+                Document document = new Document(reader.GetPageSizeWithRotation(1));
+                // step 2: we create a writer that listens to the document
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(destinationFile, FileMode.Create));
+                // step 3: we open the document
+                document.Open();
+                PdfContentByte cb = writer.DirectContent;
+                PdfImportedPage page;
+                int rotation;
+                // step 4: we add content
+                while (f < sourceFiles.Length)
+                {
+                    int i = 0;
+                    while (i < n)
+                    {
+                        i++;
+                        document.SetPageSize(reader.GetPageSizeWithRotation(i));
+                        document.NewPage();
+                        page = writer.GetImportedPage(reader, i);
+                        rotation = reader.GetPageRotation(i);
+                        if (rotation == 90 || rotation == 270)
+                        {
+                            cb.AddTemplate(page, 0, -1f, 1f, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                        }
+                        else
+                        {
+                            cb.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+                        }
+                        //Console.WriteLine("Processed page " + i);
+                    }
+                    f++;
+                    if (f < sourceFiles.Length)
+                    {
+                        reader = new PdfReader(sourceFiles[f]);
+                        // we retrieve the total number of pages
+                        n = reader.NumberOfPages;
+                        //Console.WriteLine("There are " + n + " pages in the original file.");
+                    }
+                }
+                // step 5: we close the document
+                document.Close();
+            }
+            catch (Exception e)
+            {
+                string strOb = e.Message;
+            }
+        }
+
+
+        void ConvertJPG2PDF(string jpgfile, string pdf)
+        {
+            var document = new Document(iTextSharp.text.PageSize.A4, 25, 25, 25, 25);
+            using (var stream = new FileStream(pdf, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                using (var imageStream = new FileStream(jpgfile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var image = iTextSharp.text.Image.GetInstance(imageStream);
+                    if (image.Height > iTextSharp.text.PageSize.A4.Height - 25)
+                    {
+                        image.ScaleToFit(iTextSharp.text.PageSize.A4.Width - 25, iTextSharp.text.PageSize.A4.Height - 25);
+                    }
+                    else if (image.Width > iTextSharp.text.PageSize.A4.Width - 25)
+                    {
+                        image.ScaleToFit(iTextSharp.text.PageSize.A4.Width - 25, iTextSharp.text.PageSize.A4.Height - 25);
+                    }
+                    image.Alignment = iTextSharp.text.Image.ALIGN_MIDDLE;
+                    document.Add(image);
+                }
+
+                document.Close();
+            }
+        }
+
+        private void btnAddJPG_Click(object sender, EventArgs e)
+        {
+            var OfD = new OpenFileDialog();
+            OfD.Filter = "Images (*.JPG)|*.JPEG;*.JPG;*.PNG";
+            OfD.Multiselect = true;
+            OfD.Title = "MR PDF File - Select Images";
+            if (OfD.ShowDialog(this) == DialogResult.OK)
+            {
+                foreach (var f1 in OfD.FileNames)
+                {
+                    FileList.Items.Add(f1);
+                }
+            }
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            //FileList
+            if (FileList.SelectedItems.Count > 0)
+            {
+                if (FileList.SelectedItems[0].Index > 0)
+                {
+                    var i = FileList.SelectedItems[0].Index;
+                    var FF = FileList.Items[i - 1].Text;
+                    var F2 = FileList.Items[i].Text;
+
+                    FileList.Items[i - 1].Text = F2;
+                    FileList.Items[i].Text = FF;
+
+                    FileList.SelectedItems.Clear();
+                }
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            //FileList
+            if (FileList.SelectedItems.Count > 0)
+            {
+                var i = FileList.SelectedItems[0].Index;
+                if (i < FileList.Items.Count - 1)
+                {
+
+                    var FF = FileList.Items[i + 1].Text;
+                    var F2 = FileList.Items[i].Text;
+
+                    FileList.Items[i + 1].Text = F2;
+                    FileList.Items[i].Text = FF;
+
+                    FileList.SelectedItems.Clear();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (FileList.SelectedItems.Count > 0)
+            {
+                FileList.Items.RemoveAt(FileList.SelectedItems[0].Index);
+            }
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FileList_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (FileList.SelectedItems.Count > 0)
+            {
+                var FileName = FileList.Items[FileList.SelectedItems[0].Index].Text;
+                pPreview.Image = System.Drawing.Image.FromFile(FileName);
+                lblsize.Text = pPreview.Image.Width + " x " + pPreview.Image.Height;
+            }
+            else
+            {
+                pPreview.Image = null;
+                lblsize.Text = "...";
+            }
+        }
+
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+            FileList.Items.Clear();
+            pPreview.Image = null;
+            lblsize.Text = "...";
+        }
+    }
+}
