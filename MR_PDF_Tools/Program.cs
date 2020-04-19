60f99ab2bb22c5719e47bf4c9ea368c767df59f2
Mauro Rilla <mauro@rilla.it>
2020-04-19 15:05:41 +0200

    .

--- MR_PDF_Tools/Program.cs
+++ MR_PDF_Tools/Program.cs
@@ -8,12 +8,10 @@ namespace MR_PDF_Tools
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+          [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
